7edd33068996d3db477fb961dbedd301bb1e0832
Ryan Rutledge <ryan.rutledge.dev@gmail.com>
2016-08-30 01:23:07 +0000

    Add scripts.js

--- routes/router.js
+++ routes/router.js
@@ -3,5 +3,9 @@ var router = require('express').Router();
 router.get(['/', 'index', 'index.html'], function(req, res) {
     res.sendFile('index.html', {root: './public'});
 });
+router.get('/scripts.js', function(req, res) {
+    res.sendFile('scripts.js', {root: './public'});
+});
+
 
 module.exports = router;
